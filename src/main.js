07a07bff046d7956c1acf59adb6654f3390654cd
destroyko <mr.destroyko@gmail.com>
2018-12-09 14:36:53 +0200

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-Vue.use(VueAxios, axios);
+import './plugins/vuetify'
+import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    // router,
-    components: { App },
-    template: '<App/>',
-    data: {
-        message: 'hello'
-    }
-
-})
+  render: h => h(App),
+}).$mount('#app')
